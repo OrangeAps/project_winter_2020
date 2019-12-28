885ce52d0d95a100f1d1e0d290cef14861635b38
OrangeAps <ilya.gorobievskij@bk.ru>
2019-12-28 17:17:46 +0300

    надо чтото написать

---
+++ game_script.py
@@ -0,0 +1,7 @@
+import pygame
+
+def load():
+    main()
+
+def main():
+    pass
---
+++ load_image.py
@@ -0,0 +1,14 @@
+import os, pygame
+
+
+def load_image(name, ColorKey=None):
+    fullname = os.path.join('pictures', name)
+    image = pygame.image.load(fullname)
+    if ColorKey is not None:
+        if ColorKey == -1:
+            ColorKey = image.get_at((0, 0))
+        image = image.convert()
+        image.set_colorkey(ColorKey)
+    else:
+        image = image.convert_alpha()
+    return image
